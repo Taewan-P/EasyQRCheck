513a45035faa1193c65da515b389e9d33bcc0472
Taewan Park <swimingtw@gmail.com>
2020-11-04 10:54:17 +0900

    Add cookies to the database to keep cookies when relaunch

--- app/src/main/java/page/chungjungsoo/easyqr/activities/MainActivity.kt
+++ app/src/main/java/page/chungjungsoo/easyqr/activities/MainActivity.kt
@@ -9,28 +9,40 @@ import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import page.chungjungsoo.easyqr.R
+import page.chungjungsoo.easyqr.database.MyCookieDatabaseHelper
 
-
-private var cookie : String = ""
 
 class MainActivity : AppCompatActivity() {
+    var cookieDBHandler : MyCookieDatabaseHelper? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        cookieDBHandler = MyCookieDatabaseHelper(this)
 
-        logoutBtn.visibility = View.GONE
+        val cookies : String = cookieDBHandler!!.getCookies()
+
+        if (cookies != "") {
+            loginBtn.visibility = View.GONE
+            Log.d("LOAD SUCCESSFUL", "SUCCESSFULLY LOADED EXISTING COOKIES")
+        }
+        else { logoutBtn.visibility = View.GONE }
+
+
+
         loginBtn.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivityForResult(intent, 101)
         }
 
         logoutBtn.setOnClickListener {
-            cookie = ""
-            Toast.makeText(this, "Logged out.", Toast.LENGTH_SHORT).show()
-            loginBtn.visibility = View.VISIBLE
-            logoutBtn.visibility = View.GONE
+            val del = cookieDBHandler!!.deleteCookies()
+            if (del) {
+                Toast.makeText(this, "Logged out.", Toast.LENGTH_SHORT).show()
+                Log.d("DELETE SUCCESSFUL", "SUCCESSFULLY DELETED COOKIES")
+                loginBtn.visibility = View.VISIBLE
+                logoutBtn.visibility = View.GONE
+            }
         }
-
 
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -38,12 +50,36 @@ class MainActivity : AppCompatActivity() {
         if (resultCode == Activity.RESULT_OK) {
             when (requestCode) {
                 101 -> {
-                    cookie = data!!.getStringExtra("cookie").toString()
-                    Toast.makeText(applicationContext, "Logged in successfully.", Toast.LENGTH_SHORT).show()
-                    Log.d("Loaded Cookie", cookie)
-                    loginBtn.visibility = View.GONE
-                    logoutBtn.visibility = View.VISIBLE
+                    val cookie = data!!.getStringExtra("cookie")?.toString()
+                    val cookieArr = cookie!!.split("; ").toList()
+                    var jkl = ""
+                    var aut = ""
+                    var ses = ""
+                    for (name in cookieArr) {
+                        when (name.substring(0..6)) {
+                            "NID_JKL" -> {
+                                jkl = name.substring(8 until name.length)
+                            }
+                            "NID_AUT" -> {
+                                aut = name.substring(8 until name.length)
+                            }
+                            "NID_SES" -> {
+                                ses = name.substring(8 until name.length)
+                            }
+                        }
+                    }
 
+                    val added = cookieDBHandler!!.addCookies(jkl, aut, ses)
+                    if (added) {
+                        Log.d("TEST","$jkl, $aut, $ses")
+                        Toast.makeText(applicationContext, "Logged in successfully.", Toast.LENGTH_SHORT).show()
+                        Log.d("ADD SUCCESSFUL", "SUCCESSFULLY ADDED COOKIES TO THE DATABASE.")
+                        loginBtn.visibility = View.GONE
+                        logoutBtn.visibility = View.VISIBLE
+                    }
+                    else {
+                        Log.e("ADDING ERROR", "ERROR ADDING COOKIES TO DB")
+                    }
                 }
             }
         }
--- app/src/main/java/page/chungjungsoo/easyqr/database/MyCookieDatabaseHelper.kt
+++ app/src/main/java/page/chungjungsoo/easyqr/database/MyCookieDatabaseHelper.kt
@@ -1,4 +1,87 @@
 package page.chungjungsoo.easyqr.database
 
-class MyCookieDatabaseHelper {
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+
+class MyCookieDatabaseHelper(context: Context)
+    : SQLiteOpenHelper(context,
+    DB_NAME, null, DB_VERSION) {
+
+    companion object {
+        private val DB_NAME = "CookieDB"
+        private val DB_VERSION = 1
+        private val ID = "id"
+        private val TABLE_NAME = "cookielist"
+        private val NID_JKL = "jkl_cookie"
+        private val NID_AUT = "aut_cookie"
+        private val NID_SES = "ses_cookie"
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        val createTable =
+                "CREATE TABLE $TABLE_NAME" +
+                "($ID INTEGER PRIMARY KEY, $NID_JKL TEXT, $NID_AUT TEXT, $NID_SES TEXT)"
+        db?.execSQL(createTable)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) { }
+
+    fun addCookies(jkl: String, aut: String, ses: String) : Boolean {
+        val db = this.writableDatabase
+        val values = ContentValues()
+        values.put(NID_JKL, jkl)
+        values.put(NID_AUT, aut)
+        values.put(NID_SES, ses)
+
+        val success = db.insert(TABLE_NAME, null, values)
+        db.close()
+        return (Integer.parseInt("$success") != -1)
+    }
+
+    fun updateCookies(jkl: String, aut: String, ses: String) : Boolean {
+        return true
+    }
+
+    fun getCookies() : String {
+        val db = readableDatabase
+        val selectALLQuery = "SELECT * FROM $TABLE_NAME"
+        val cursor = db.rawQuery(selectALLQuery, null)
+        val jkl : String
+        val aut : String
+        val ses : String
+        var result = ""
+
+        if (cursor == null) {
+            Log.d("DATABASE_NULL", "CURSOR IS NULL")
+            return result
+        }
+
+        if (cursor.moveToFirst()) {
+                jkl = cursor.getString(cursor.getColumnIndex(NID_JKL))
+                aut = cursor.getString(cursor.getColumnIndex(NID_AUT))
+                ses = cursor.getString(cursor.getColumnIndex(NID_SES))
+                result = "NID_JKL=$jkl; NID_AUT=$aut; NID_SES=$ses;"
+        }
+        else {
+            Log.e("DATABASE_ERROR", "GETTING COOKIES HAS FAILED.")
+        }
+        cursor.close()
+        db.close()
+
+        return result
+    }
+
+    fun deleteCookies() : Boolean {
+        val db = this.writableDatabase
+        val query = db.rawQuery("DELETE FROM $TABLE_NAME", null)
+        val num = query.count
+        query.close()
+        db.close()
+        return num == 0
+    }
+
+
 }
