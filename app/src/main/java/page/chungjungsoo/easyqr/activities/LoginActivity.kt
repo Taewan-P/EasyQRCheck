3b7c179e6c1536d3750d4d03261893cfb2499b3e
Taewan Park <swimingtw@gmail.com>
2020-11-10 00:38:58 +0900

    Add safety coding to QR authentication

--- app/src/main/java/page/chungjungsoo/easyqr/activities/LoginActivity.kt
+++ app/src/main/java/page/chungjungsoo/easyqr/activities/LoginActivity.kt
@@ -4,7 +4,9 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.webkit.*
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
 import page.chungjungsoo.easyqr.R
@@ -19,18 +21,44 @@ class LoginActivity : AppCompatActivity() {
         val cookieManager: CookieManager = CookieManager.getInstance()
         cookieManager.setAcceptCookie(true)
 
-        loginWebView.loadUrl("https://nid.naver.com/nidlogin.login")
         loginWebView.settings.javaScriptEnabled = true
-        loginWebView.settings.domStorageEnabled = false
-        loginWebView.webViewClient = object : WebViewClient() {
-            override fun doUpdateVisitedHistory(view: WebView?, url: String?, isReload: Boolean) {
-                if (url.toString() == "https://m.naver.com/" || url.toString() == "https://www.naver.com/") {
-                    cookies = cookieManager.getCookie(view?.url).toString()
-                    val intent = Intent()
-                    intent.putExtra("cookie", cookies)
-                    setResult(Activity.RESULT_OK, intent)
-                    finish()
+        loginWebView.settings.domStorageEnabled = true
+
+        val loginType = intent.getStringExtra("LOGIN_TYPE")
+
+        if (loginType == "L") {
+            authBtn.visibility = View.GONE
+            loginWebView.loadUrl("https://nid.naver.com/nidlogin.login")
+            loginWebView.webViewClient = object : WebViewClient() {
+                override fun doUpdateVisitedHistory(view: WebView?, url: String?, isReload: Boolean) {
+                    if (url.toString() == "https://m.naver.com/" || url.toString() == "https://www.naver.com/") {
+                        cookies = cookieManager.getCookie(view?.url).toString()
+                        val intent = Intent()
+                        intent.putExtra("cookie", cookies)
+                        setResult(Activity.RESULT_OK, intent)
+                        finish()
+                    }
                 }
+            }
+        }
+        else if (loginType == "P") {
+            // Phone Verification
+            supportActionBar?.title = "NAVER Authentication Page"
+            authBtn.visibility = View.VISIBLE
+            Toast.makeText(this, "개인정보 제공 동의 및 전화번호 인증을 완료후 인증 완료 버튼을 눌러주세요.", Toast.LENGTH_LONG).show()
+            loginWebView.loadUrl("https://nid.naver.com/login/privacyQR")
+            loginWebView.webViewClient = object : WebViewClient() {
+                override fun shouldOverrideUrlLoading(
+                    view: WebView?,
+                    request: WebResourceRequest?
+                ): Boolean {
+                    return false
+                }
+            }
+            authBtn.setOnClickListener {
+                val intent = Intent(this, PopupActivity::class.java)
+                startActivity(intent)
+                finishAndRemoveTask()
             }
         }
 
--- app/src/main/java/page/chungjungsoo/easyqr/activities/PopupActivity.kt
+++ app/src/main/java/page/chungjungsoo/easyqr/activities/PopupActivity.kt
@@ -1,6 +1,7 @@
 package page.chungjungsoo.easyqr.activities
 
 import android.app.Activity
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
@@ -10,6 +11,7 @@ import android.view.View
 import android.view.Window
 import android.webkit.CookieManager
 import android.widget.Toast
+import androidx.lifecycle.Lifecycle
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.Headers
 import com.github.kittinunf.result.Result
@@ -21,6 +23,7 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
 import java.io.ByteArrayInputStream
+import java.net.UnknownHostException
 import java.util.*
 
 class PopupActivity : Activity() {
@@ -41,6 +44,7 @@ class PopupActivity : Activity() {
         val url = "https://nid.naver.com/login/privacyQR"
 
         qrImageView.visibility = View.INVISIBLE
+        timeText.visibility = View.INVISIBLE
         if (cookies != "") {
             scope.launch(Dispatchers.Default) {
                 val (request, response, result) = Fuel.get(url)
@@ -54,19 +58,37 @@ class PopupActivity : Activity() {
                 when (result) {
                     is Result.Failure -> {
                         val ex = result.getException()
-                        println(ex)
+                        if (ex.exception is UnknownHostException) {
+                            runOnUiThread {
+                                Toast.makeText(baseContext, "오류! 인터넷에 연결되어있지 않습니다.", Toast.LENGTH_SHORT).show()
+                                finishAndRemoveTask()
+                            }
+                        }
+                        println(ex.exception)
                     }
                     is Result.Success -> {
                         val doc: Document = Jsoup.parse(result.get())
-                        val src = doc.getElementById("qrImage").attr("src")
-                        val base64val = src.split(", ")[1]
-                        val decoded = Base64.getDecoder().decode(base64val)
-                        val decodedByte: Bitmap =
-                            BitmapFactory.decodeByteArray(decoded, 0, decoded.size)
-                        runOnUiThread {
-                            loading.visibility = View.GONE
-                            qrImageView.visibility = View.VISIBLE
-                            qrImageView.setImageBitmap(decodedByte)
+                        try {
+                            val src = doc.getElementById("qrImage").attr("src")
+                            val base64val = src.split(", ")[1]
+                            val decoded = Base64.getDecoder().decode(base64val)
+                            val decodedByte: Bitmap =
+                                BitmapFactory.decodeByteArray(decoded, 0, decoded.size)
+                            runOnUiThread {
+                                loading.visibility = View.GONE
+                                qrImageView.visibility = View.VISIBLE
+                                qrImageView.setImageBitmap(decodedByte)
+                                timeText.visibility = View.VISIBLE
+                            }
+
+                        } catch (e: NullPointerException){
+                            runOnUiThread {
+                                Toast.makeText(baseContext, "개인정보 제공 동의 및 전화번호 인증이 필요합니다.", Toast.LENGTH_LONG).show()
+                                val intent = Intent(baseContext, LoginActivity::class.java)
+                                intent.putExtra("LOGIN_TYPE", "P")
+                                startActivity(intent)
+                                finishAndRemoveTask()
+                            }
                         }
                     }
                 }
@@ -74,11 +96,11 @@ class PopupActivity : Activity() {
         }
         else {
             Toast.makeText(this, "로그인을 먼저 해주세요.", Toast.LENGTH_LONG).show()
-            finish()
+            finishAndRemoveTask()
         }
 
         okBtn.setOnClickListener {
-            finish()
+            finishAndRemoveTask()
         }
 
     }
