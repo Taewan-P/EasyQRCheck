3e8f092412afdc808ef6da1d068fdd47ac11b87c
Taewan Park <swimingtw@gmail.com>
2020-11-09 18:01:24 +0900

    Implement QR img load

--- app/src/main/java/page/chungjungsoo/easyqr/activities/PopupActivity.kt
+++ app/src/main/java/page/chungjungsoo/easyqr/activities/PopupActivity.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import android.view.View
 import android.view.Window
 import android.webkit.CookieManager
+import android.widget.Toast
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.Headers
 import com.github.kittinunf.result.Result
@@ -40,29 +41,40 @@ class PopupActivity : Activity() {
         val url = "https://nid.naver.com/login/privacyQR"
 
         qrImageView.visibility = View.INVISIBLE
-        scope.launch(Dispatchers.Default) {
-            val (request, response, result) = Fuel.get(url)
-                .appendHeader(Headers.COOKIE to cookieArr[0], Headers.COOKIE to cookieArr[1], Headers.COOKIE to cookieArr[2])
-                .responseString()
+        if (cookies != "") {
+            scope.launch(Dispatchers.Default) {
+                val (request, response, result) = Fuel.get(url)
+                    .appendHeader(
+                        Headers.COOKIE to cookieArr[0],
+                        Headers.COOKIE to cookieArr[1],
+                        Headers.COOKIE to cookieArr[2]
+                    )
+                    .responseString()
 
-            when (result) {
-                is Result.Failure -> {
-                    val ex = result.getException()
-                    println(ex)
-                }
-                is Result.Success -> {
-                    val doc: Document = Jsoup.parse(result.get())
-                    val src = doc.getElementById("qrImage").attr("src")
-                    val base64val = src.split(", ")[1]
-                    val decoded = Base64.getDecoder().decode(base64val)
-                    val decodedByte : Bitmap = BitmapFactory.decodeByteArray(decoded, 0, decoded.size)
-                    runOnUiThread {
-                        loading.visibility = View.GONE
-                        qrImageView.visibility = View.VISIBLE
-                        qrImageView.setImageBitmap(decodedByte)
+                when (result) {
+                    is Result.Failure -> {
+                        val ex = result.getException()
+                        println(ex)
+                    }
+                    is Result.Success -> {
+                        val doc: Document = Jsoup.parse(result.get())
+                        val src = doc.getElementById("qrImage").attr("src")
+                        val base64val = src.split(", ")[1]
+                        val decoded = Base64.getDecoder().decode(base64val)
+                        val decodedByte: Bitmap =
+                            BitmapFactory.decodeByteArray(decoded, 0, decoded.size)
+                        runOnUiThread {
+                            loading.visibility = View.GONE
+                            qrImageView.visibility = View.VISIBLE
+                            qrImageView.setImageBitmap(decodedByte)
+                        }
                     }
                 }
             }
+        }
+        else {
+            Toast.makeText(this, "로그인을 먼저 해주세요.", Toast.LENGTH_LONG).show()
+            finish()
         }
 
         okBtn.setOnClickListener {
